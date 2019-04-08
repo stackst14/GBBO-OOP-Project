11ad5664e25e14202f0510d548f01a8acbf2a398
codik <codik@DESKTOP-L0KBG6R>
2019-04-07 22:04:51 -0500

    Edited ArrayList bakers

---
+++ src/com/javaproject/gbbo/Main.java
@@ -0,0 +1,21 @@
+package com.javaproject.gbbo;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+       // ArrayList<Baker> Bakers;
+       // Bakers = new ArrayList<Baker>();
+       
+         ArrayList<String> bakers = new ArrayList<String>(Arrays.asList("alex", "ruby"));
+        // System.out.println(bakers);
+       
+        Season Season1 = new Season(1, bakers);
+        
+// ArrayList<String> bakers = new ArrayList<String>(Arrays.asList("alex", "ruby"));
+       // System.out.println(bakers);
+    
+    }  
+    
+}
