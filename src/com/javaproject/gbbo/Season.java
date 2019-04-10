2a3cee2b1e29098b550cf088976e394257acb6b7
codik <codik@DESKTOP-L0KBG6R>
2019-04-09 23:25:38 -0500

    Added commands for the Episode class to be called in the Season class

--- src/com/javaproject/gbbo/Season.java
+++ src/com/javaproject/gbbo/Season.java
@@ -2,24 +2,17 @@ package com.javaproject.gbbo;
 import java.util.ArrayList;
 
 public class Season {
-    private  ArrayList<String> bakers; //An arraylist that will hold all the Bakers information.
     private int seasonNum; // The season number.
     
     //Constructor that will create a Season with all the bakers
-        public Season(int season, ArrayList<String> bakers){
+        public Season(int season){
         this.seasonNum = season;
-        this.bakers = bakers;
-        
-        System.out.println("Inside season class constructor");
+       
         System.out.println("season " + seasonNum);
-        System.out.println("list of bakers in season class" + bakers);
-    }
-    
-    public void setSeason(int season){//Set the season if for some reason the season was set wrong
-        this.seasonNum = season;
-    }
-    
-    public int getSeason(){// Get the current season
-        return this.seasonNum;
+        Episode EpisodeCreate = new Episode();
+        EpisodeCreate.printEpisodeNumAndName();
+       
+        System.out.println("Inside season class constructor");
     }
 }
+    
